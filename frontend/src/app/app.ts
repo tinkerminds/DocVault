62cc15ed87ab0b0e36f2c1d0fef2c983d1f1b584
Gaurav Prajapati <gauravprajapati9370@gmail.com>
2026-02-17 15:49:09 +0530

    feat(home): complete UI for homepage

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,11 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar.component/navbar.component';
+import { FooterComponent } from './components/footer.component/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.html',
-  styleUrl: './app.scss'
+  styleUrl: './app.scss',
 })
 export class App {
   protected readonly title = signal('DocVault.Web');
---
+++ frontend/src/app/components/navbar.component/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [CommonModule, RouterLink],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss',
+})
+export class NavbarComponent {}
